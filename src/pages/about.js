a6483535a77a3f87687134276dda6e69a3ade144
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-22 14:55:23 -0600

    Add about page.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const aboutPage = () => {
+  return(
+    <Layout>
+      <h1>About Me</h1>
+    </Layout>
+  )
+}
+
+export default aboutPage
