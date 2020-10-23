1271324cf9f9d490cadd4dcbe89a125ac4a53f2e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-23 17:28:03 -0600

    Add ability to create blog posts from .md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,53 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.createPages = ({ actions, graphql}) => {
+  const { createPage } = actions;
+
+  const postTemplate = path.resolve('src/templates/postTemplate.js');
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: postTemplate,
+        context: { slug: node.fields.slug },
+      });
+    });
+  });
+};
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages`});
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
