0b755392190ecf5f67ae84edaf05c3ae696d6e6e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-23 15:02:05 -0600

    Remove some inline styling from layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,14 +30,12 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          // maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <main className="site-content">{children}</main>
+        <footer className="site-content">
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
