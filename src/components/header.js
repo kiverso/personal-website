f1b88238fadb929b522d73b9ad93ff3f23d06428
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-23 22:11:30 -0600

    Remove references to nav-link

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,15 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import logo from "../images/logo.svg"
-import NavLink from "./nav_link"
 
 const Header = ({ siteTitle }) => (
-  <nav className="header">
-    <div className="nav-content site-content"
-      style={{
-        // margin: `0 auto`,
-        // maxWidth: 960,
-        // maxHeight: '2.5em',
-        // paddingLeft: 10
-      }}
-    >
-      <ul>
-        <li>
-          <Link to="/">
-            <img src = {logo} alt="site logo" width="40" height="40" className="nav-logo"></img>
-          </Link>
-        </li>
-        <li>
-          <NavLink linkText="Blog" linkDestination="/blog">
-          </NavLink>
-        </li>
-      </ul>
+  <header className="header">
+    <div className="nav-content site-content">
+      <Link to="/">
+        <img src = {logo} alt="site logo" width="40" height="40" className="nav-logo"></img>
+      </Link>
     </div>
-  </nav>
+  </header>
 )
 
 Header.propTypes = {
