af3859386b9646769a8c42f8773e315ccbd4fbc9
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-23 20:41:23 -0600

    Add navigation link for blog.

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import logo from "../images/logo.svg"
+import NavLink from "./nav_link"
 
 const Header = ({ siteTitle }) => (
   <nav className="header">
@@ -13,11 +14,17 @@ const Header = ({ siteTitle }) => (
         // paddingLeft: 10
       }}
     >
-      <Link
-        to="/"
-      >
-        <img src = {logo} alt="site logo" width="40" height="40" class="nav-logo"></img>
-      </Link>
+      <ul>
+        <li>
+          <Link to="/">
+            <img src = {logo} alt="site logo" width="40" height="40" className="nav-logo"></img>
+          </Link>
+        </li>
+        <li>
+          <NavLink linkText="Blog" linkDestination="/blog">
+          </NavLink>
+        </li>
+      </ul>
     </div>
   </nav>
 )
