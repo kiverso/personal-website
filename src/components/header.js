2b154d626826beb093a7e51eb14cbd06e9cc756d
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-23 23:17:36 -0600

    Adjust styling.

--- src/components/header.js
+++ src/components/header.js
@@ -7,12 +7,14 @@ const Header = ({ siteTitle, menuLinks }) => (
   <header className="header">
     <div className="nav-content site-content">
       <Link to="/">
-        <img src = {logo} alt="site logo" width="40" height="40" className="nav-logo"></img>
+        <img src = {logo} alt="site logo" width="40" height="40" className="nav-link"></img>
       </Link>
       <nav>
         <ul>
           {menuLinks.map(link => (
-            <li key={link.name}>
+            <li key={link.name}
+             className="nav-link"
+            >
               <Link to={link.link}>
                 {link.name}
               </Link>
