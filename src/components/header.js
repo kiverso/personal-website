d372b41da95c60cfe4030caf3b2de14b7c4c79f3
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-26 12:16:09 -0600

    Move nav links to right side of page.

--- src/components/header.js
+++ src/components/header.js
@@ -4,25 +4,17 @@ import React from "react"
 import logo from "../images/logo.svg"
 
 const Header = ({ siteTitle, menuLinks }) => (
-  <header className="header">
-    <div className="nav-content site-content">
-      <Link to="/">
-        <img src = {logo} alt="site logo" width="40" height="40" className="nav-link"></img>
-      </Link>
-      <nav>
-        <ul>
-          {menuLinks.map(link => (
-            <li key={link.name}
-             className="nav-link"
-            >
-              <Link to={link.link}>
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+  <header className="header site-content">
+    <Link to="/">
+      <img src = {logo} alt="site logo" width="40" height="40" className="nav-link logo"></img>
+    </Link>
+    <nav className="nav">
+      {menuLinks.map(link => (
+        <Link to={link.link} key={link.name} className="nav-link">
+          {link.name}
+        </Link>
+      ))}
+    </nav>
   </header>
 )
 
