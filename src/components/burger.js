9cd558281ebde6ff6209a2ace64e799e67e75af5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-10-28 00:24:05 -0600

    Add animation for burger turning into x when open.

--- src/components/burger.js
+++ src/components/burger.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { bool, func } from 'prop-types';
+import { CSSTransition } from "react-transition-group"
 
 const Burger = ({open, setOpen}) => {
   return(
     <button className="burger" open={open} onClick={() => setOpen(!open)}>
-      <div className="top-bun"/>
-      <div className="middle-bun"/>
-      <div className="bottom-bun"/>
+      <CSSTransition in={ open } timeout={250}
+        classNames="burgerTopAnimation">
+        <div className="top-bun"/>
+      </CSSTransition>
+      <CSSTransition in={ open } timeout={250}
+        classNames="burgerMiddleAnimation">
+        <div className="middle-bun"/>
+      </CSSTransition>
+      <CSSTransition in={ open } timeout={250}
+        classNames="burgerBottomAnimation">
+        <div className="bottom-bun"/>
+      </CSSTransition>
   </button>
   )
 }
