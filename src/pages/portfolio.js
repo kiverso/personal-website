683d403fa5e5d97c42fa2bf6fdb99401f78f9903
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-11-05 22:22:22 -0700

    Add images to portfolio page.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,14 +1,47 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Img from "gatsby-image"
 
-const portfolioPage = () => {
-  return(
+const portfolioPage = ({data}) => (
     <Layout>
       <SEO title="Portfolio"/>
       <h1>My Projects</h1>
+      <div className="all-projects">
+        { data.allProjectsJson.edges.map(project => (
+          <div key={project.node.id} className="project">
+            <div className="project-image">
+              <Img fluid={project.node.image.childImageSharp.fluid} />
+            </div>
+
+          </div>
+        ))}
+      </div>
     </Layout>
   )
-}
 
 export default portfolioPage
+
+export const projectsQuery = graphql`
+  query {
+    allProjectsJson(sort: { order: DESC, fields: [date] }) {
+      edges {
+        node {
+          id
+          title
+          date
+          description
+          demo
+          repo
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
